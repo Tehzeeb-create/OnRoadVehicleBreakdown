2501e189579f56c610a134c52a7907cd656cb0ad
Tehzeeb-create <73227426+Tehzeeb-create@users.noreply.github.com>
2023-11-24 00:04:28 +0500

    24 Nov 2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,11 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
     testImplementation("junit:junit:4.13.2")
     implementation("com.google.android.gms:play-services-maps:17.0.1")
-    implementation ("com.google.android.gms:play-services-maps:17.0.1")
+    implementation("com.google.android.gms:play-services-maps:17.0.1")
     implementation ("com.google.android.gms:play-services-location:18.0.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 
 
 
--- app/src/main/java/com/example/onroadvehiclebreakdown/CustomerDashboardActivity.kt
+++ app/src/main/java/com/example/onroadvehiclebreakdown/CustomerDashboardActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -17,17 +16,20 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import android.util.Log
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import kotlin.random.Random
 
 class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
 
@@ -37,7 +39,7 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
     private val DEFAULT_ZOOM = 15.0f
     private val TAG = "CustomerDashboardActivity"
-    private lateinit var map: GoogleMap
+    private var map: GoogleMap? = null
     private val LOCATION_PERMISSION_REQUEST = 1
 
     private lateinit var saveButton: Button
@@ -48,9 +50,8 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var carEditText: EditText
     private lateinit var addressEditText: EditText
 
-    private val defaultLocation = LatLng(37.7749, -122.4194)
+    private val defaultLocation = LatLng(33.4323, 73.02182)
 
-    private val userLocation = LatLng(33.000, 77.000)
 
 
 
@@ -60,6 +61,8 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
 
         // Check and request location permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -72,17 +75,22 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
         databaseReference = FirebaseDatabase.getInstance().reference
         fusedLocationProviderClient = FusedLocationProviderClient(this)
 
-
+        val myInfoButton = findViewById<Button>(R.id.myInfoButton)
+        val myInfoSection = findViewById<LinearLayout>(R.id.myInfoSection)
+        val CurrentLocation = findViewById<ImageButton>(R.id.currentLoc)
 
         // Initialize UI elements
         saveButton = findViewById(R.id.saveButton)
         sosButton = findViewById(R.id.sosButton)
-        closestPersonTextView = findViewById(R.id.closestPersonTextView)
+        //closestPersonTextView = findViewById(R.id.closestPersonTextView)
         nameEditText = findViewById(R.id.nameEditText)
         phoneEditText = findViewById(R.id.phoneEditText)
         carEditText = findViewById(R.id.carEditText)
         addressEditText = findViewById(R.id.addressEditText)
 
+        CurrentLocation.setOnClickListener {
+            moveToCurrentLocation()
+        }
         // Set onClickListener for Save Button
         saveButton.setOnClickListener {
             val name = nameEditText.text.toString()
@@ -91,7 +99,7 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
             val address = addressEditText.text.toString()
 
             val userId = auth.currentUser?.uid // Get the current user's unique ID
-
+            myInfoSection.visibility = View.INVISIBLE
             userId?.let { uid ->
                 // Reference to the "users" node in your database
                 val usersRef = databaseReference.child("users").child(uid)
@@ -100,6 +108,7 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
                 usersRef.addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
                         val userData = dataSnapshot.getValue(User::class.java)
+                        updateUserLocationOnMap()
 
                         userData?.let { user ->
 
@@ -111,20 +120,16 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
                                 phone = phone,
                                 carNumber = carNumber,
                                 address = address,
-                                location = userLocation
+                                location = null
                             )
 
-                            // Update the user's data in the database
                             usersRef.setValue(updatedUser)
                                 .addOnSuccessListener {
-                                    // Update successful
                                     Toast.makeText(this@CustomerDashboardActivity, "User information updated", Toast.LENGTH_SHORT).show()
                                 }
                                 .addOnFailureListener {
-                                    // Handle update failure
                                     Toast.makeText(this@CustomerDashboardActivity, "Failed to update user information", Toast.LENGTH_SHORT).show()
                                 }
-                            updateUserLocationOnMap()
 
                         }
                     }
@@ -138,19 +143,26 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
 
         }
 
-        // Set onClickListener for SOS Button
+        myInfoButton.setOnClickListener {
+            if (myInfoSection.visibility == View.VISIBLE) {
+                myInfoSection.visibility = View.INVISIBLE
+            } else {
+                myInfoSection.visibility = View.VISIBLE
+            }
+        }
         sosButton.setOnClickListener {
-            // Display SOS message or perform SOS action
-            closestPersonTextView.text = "SOS, help me"
+            //closestPersonTextView.text = "SOS, help me"
         }
 
-        // Calculate and display closest person's name (implement this logic)
+        // Calculate and display closest person's name
         calculateClosestPerson()
     }
 
+    private fun moveToCurrentLocation() {
+        updateUserLocationOnMap()
+    }
     private fun calculateClosestPerson() {
         // This is where you implement the logic to find the closest person and update the UI
-        // Example: closestPersonTextView.text = "Closest person: John Doe"
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -166,19 +178,23 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        map.uiSettings.isMyLocationButtonEnabled = true
-        updateUserLocationOnMap()
+        map?.uiSettings?.isMyLocationButtonEnabled = true
+        enableMyLocation()
+        moveToCurrentLocation()
+
+
+        val userLocation = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+        addRandomMarkersAroundUserLocation(userLocation)
     }
 
     private fun enableMyLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            map.isMyLocationEnabled = true
+            map?.isMyLocationEnabled = true
         }
     }
 
 
-    public fun updateUserLocationOnMap() {
-        // Check for location permissions
+    fun updateUserLocationOnMap() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             val locationResult: Task<Location> = fusedLocationProviderClient.lastLocation
 
@@ -188,9 +204,8 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
                     if (lastKnownLocation != null) {
                         val userLocation = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
                         map?.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, DEFAULT_ZOOM))
-
-                        // Update user's location in Firebase
                         updateLocationInFirebase(userLocation)
+
                     } else {
                         Log.d(TAG, "Current location is null. Using defaults.")
                         map?.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_ZOOM))
@@ -207,42 +222,50 @@ class CustomerDashboardActivity : FragmentActivity(), OnMapReadyCallback {
     }
 
     private fun updateLocationInFirebase(location: LatLng) {
-        val userId = auth.currentUser?.uid // Get the current user's unique ID
+        val userId = auth.currentUser?.uid
 
         userId?.let { uid ->
-            // Reference to the "users" node in your database
             val usersRef = databaseReference.child("users").child(uid)
+            val updateData = mapOf<String, Any>("location" to location)
+            usersRef.updateChildren(updateData)
 
-            // Fetch the existing user data
-            usersRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val userData = dataSnapshot.getValue(User::class.java)
-
-                    userData?.let { user ->
-                        // Update the user's location
-                        user.location = location
-
-                        // Update the user's data in the database
-                        usersRef.setValue(user)
-                            .addOnSuccessListener {
-                                // Update successful
-                                Toast.makeText(this@CustomerDashboardActivity, "User location updated", Toast.LENGTH_SHORT).show()
-                            }
-                            .addOnFailureListener {
-                                // Handle update failure
-                                Toast.makeText(this@CustomerDashboardActivity, "Failed to update user location", Toast.LENGTH_SHORT).show()
-                            }
-                    }
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // Handle database error
-                    Toast.makeText(this@CustomerDashboardActivity, "Failed to fetch user data", Toast.LENGTH_SHORT).show()
-                }
-            })
         }
     }
 
+    private fun addRandomMarkersAroundUserLocation(userLocation: LatLng) {
+        val numberOfMarkers = 20
+        val maxDistanceInMeters = 5000 // Adjust this distance as needed
+
+        for (i in 0 until numberOfMarkers) {
+            val randomDistance = Random.nextDouble(0.0, maxDistanceInMeters.toDouble())
+            val randomBearing = Random.nextDouble(0.0, 360.0)
+
+            val newLatLng = calculateLatLng(userLocation, randomDistance, randomBearing)
+
+            // Add a marker for the new coordinates
+            map?.addMarker(MarkerOptions().position(newLatLng).title("Mechanic $i"))
+        }
+    }
+
+    private fun calculateLatLng(startLatLng: LatLng, distance: Double, bearing: Double): LatLng {
+        val earthRadius = 6371000.0 // Earth's radius in meters
+
+        val lat1 = Math.toRadians(startLatLng.latitude)
+        val lon1 = Math.toRadians(startLatLng.longitude)
+        val angularDistance = distance / earthRadius
+
+        val lat2 = Math.asin(
+            Math.sin(lat1) * Math.cos(angularDistance) +
+                    Math.cos(lat1) * Math.sin(angularDistance) * Math.cos(Math.toRadians(bearing))
+        )
+
+        val lon2 = lon1 + Math.atan2(
+            Math.sin(Math.toRadians(bearing)) * Math.sin(angularDistance) * Math.cos(lat1),
+            Math.cos(angularDistance) - Math.sin(lat1) * Math.sin(lat2)
+        )
+
+        return LatLng(Math.toDegrees(lat2), Math.toDegrees(lon2))
+    }
 
 
 
